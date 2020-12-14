0345a93018c8605f99df42ff5c5c1c2dd3236eef
ed <eduardobarroso@edu.unifor.br>
2020-12-14 08:20:49 -0300

    * Remoção do checkPrivillege e fakeDB que agora são desnecessarios graças ao jpa e h2.  * Remoção do AnimeController, FilmeController, SerieController, para que tudo seja um ProductionModel.  * Por isso também a remoção das Classes Anime, Filme e Serie.  * Junção de Demography, Genres e Themes, na nova Classe Tags, junto disso mudanças no BD.  * Mudança de Status e Format de ENUM para String, por isso remoção do mesmo.

--- src/main/java/com/mdb/Animdb/model/productions/Status.java
+++
@@ -1,5 +0,0 @@
-package com.mdb.Animdb.model.productions;
-
-public enum Status {
-    Releasing, Finished, NotYetReleased;
-}
--- src/main/java/com/mdb/Animdb/model/productions/Themes.java
+++
@@ -1,8 +0,0 @@
-package com.mdb.Animdb.model.productions;
-
-public enum Themes {
-    Aliens, Animals, Cooking, CrossDressing, Delinquent, Demon, GenderSwap,  Ghosts, Gyaru, Harem, Mafia, Magic,
-    MarcialArts, Military, MonsterGirls, Monsters, Ninja, OfficeWorkers, Police, PostApocalyptic, Reincarnation,
-    ReverseHarem, Samurai, SchoolLife, Supernatural, Survival, TimeTravel, TraditionalGames, Vampires, VideoGame,
-    Villian, VirtualReality, Zombie;
-}
